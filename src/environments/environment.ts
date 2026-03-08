01092d1a5703dfa1779ff6474a981b7b45d7543a
Badlix <mathieu.leroux173@gmail.com>
2026-03-08 08:55:40 +0100

    fix: fix prod api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
-  // apiBaseUrl: 'http://ecirada.valorium-mc.fr:5016',
-  apiBaseUrl: 'http://127.0.0.1:5013/api', // TODO : change when dev is finish
+  apiBaseUrl: 'http://ecirada.valorium-mc.fr:5016/api',
   logHttpErrors: true,
 };
