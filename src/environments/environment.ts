896db141ae3f4277259c8d219f1844ee0ad862b2
Siwa12100 <jean.marcillac12@gmail.com>
2026-03-08 20:28:11 +0100

    test ci

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: false,
   apiBaseUrl: 'http://ecirada.valorium-mc.fr:5016',
+  // apiBaseUrl: `https://api.colors.valorium-mc.fr`,
+  // apiBaseUrl: `http://davalada.valorium-mc.fr:5013`,
   logHttpErrors: true,
 };
