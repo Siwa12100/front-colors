d7537ab1422fac47f306b07f0b328cae16c390c7
Badlix <mathieu.leroux173@gmail.com>
2026-03-08 11:18:55 +0100

    fix: add home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { LoginPage } from './components/login-page/login-page';
 import { AuthCallbackPage } from './components/auth-callback-page/auth-callback-page';
 import { HomePage } from './components/home-page/home-page';
+import { WorkspaceComponent } from './components/workspace/workspace';
 
 export const routes: Routes = [
-    { path: '', component: HomePage},
+    { path: '', component: HomePage },
     { path: 'login', component: LoginPage },
-    { path: 'auth/callback', component: AuthCallbackPage }];
+    { path: 'auth/callback', component: AuthCallbackPage },
+    { path: 'home', component: WorkspaceComponent }];
