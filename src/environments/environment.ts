f65f8610217263de2184bebfd4b43df0e985d6db
Badlix <mathieu.leroux173@gmail.com>
2026-03-10 09:25:23 +0100

    feat: add sync

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   // apiBaseUrl: 'http://ecirada.valorium-mc.fr:5016',
-  apiBaseUrl: 'http://127.0.0.1:5013',
+  //apiBaseUrl: 'http://127.0.0.1:5013',
   // apiBaseUrl: 'http://davalada.valorium-mc.fr:5013',
-  //apiBaseUrl: 'https://api.colors.valorium-mc.fr',
+  apiBaseUrl: 'https://api.colors.valorium-mc.fr',
   logHttpErrors: true,
 };
